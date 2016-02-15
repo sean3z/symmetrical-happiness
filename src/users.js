a1ad294e3468a6ae26e4bd5f8107368506f88938
sean3z <seanwragg@gmail.com>
2016-02-15 08:26:38 -0500

    collapsing scope

--- src/users.js
+++ src/users.js
@@ -7,53 +7,54 @@ const config = require('../config');
 let tests = config.tests || [];
 
 module.exports = (enabled, cb) => {
-  async.parallel(users(enabled), cb);
-};
+  async.parallel(users(), cb);
 
-function users(enabled) {
-  let users = [];
+  function users() {
+    let users = [];
 
-  for (let x = config.request.users; x > 0; x--) {
-    let user = (cb) => {
-      async.series(tasks(enabled), (err, results) => {
-        cb(err, results);
-      });
-    };
-
-    users.push(user);
-  };
-
-  return users;
-}
-
-function tasks(enabled) {
-  let tasks = [];
-  for (let test of tests) {
-    let options = {
-      hostname: 'www.google.com',
-      // hostname: config.environment.host,
-      // path: test.path,
-      method: test.method.toUpperCase()
-    };
-
-    if (enabled) {
-      options.headers = {};
-    } else {
-      options.headers = {};
-    }
-
-    for (let x = config.request.requests; x > 0; x--) {
-      let task = (cb) => {
-        setTimeout(() => {
-          request(options).then((req) => {
-            cb(null, req);
-          });
-        }, config.request.delay * 1000);
+    for (let x = config.request.users; x > 0; x--) {
+      let user = (cb) => {
+        async.series(tasks(), (err, results) => {
+          cb(err, results);
+        });
       };
 
-      tasks.push(task);
+      users.push(user);
     };
+
+    return users;
   }
 
-  return tasks;
-}
+  function tasks() {
+    let tasks = [];
+    
+    for (let test of tests) {
+      let options = {
+        hostname: 'www.google.com',
+        // hostname: config.environment.host,
+        // path: test.path,
+        method: test.method.toUpperCase()
+      };
+
+      if (enabled) {
+        options.headers = {};
+      } else {
+        options.headers = {};
+      }
+
+      for (let x = config.request.requests; x > 0; x--) {
+        let task = (cb) => {
+          setTimeout(() => {
+            request(options).then((req) => {
+              cb(null, req);
+            });
+          }, config.request.delay * 1000);
+        };
+
+        tasks.push(task);
+      };
+    }
+
+    return tasks;
+  }
+};
